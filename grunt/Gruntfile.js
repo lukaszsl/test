6b1996d257a60230724b7678c5c0c66e70affe8b
Lukasz Sliczner <lukaszsliczner@MacBook-Pro.local>
2017-05-18 21:51:29 +0000

    created branch for new feature: featContribWatch

---
+++ grunt/Gruntfile.js
@@ -0,0 +1,23 @@
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+  	sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'css/style.css': 'sass/*.sass'
+        }
+      }
+    }
+    
+  });
+  // Load the plugins tasks 
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  // Default task(s).
+  grunt.registerTask('default', ['sass']);
+};
